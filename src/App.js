c7cd9d9dde6f853eb8b9eda0df1501e18226922b
Nency-Ravaliya <ravaliyanensi@gmail.com>
2024-04-08 12:06:23 +0530

    push the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to the DevOpSky: Cloud & DevOps Journey</h2>
+          <button 
+            style={{
+              padding: '20px 30px',
+              fontSize: '25px',
+              backgroundColor: '#007bff',
+              color: '#fff',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+              transition: 'background-color 0.3s ease, transform 0.2s ease',
+            }}
+            onClick={() => window.location.href = 'https://www.nensiravaliya.me'}
+            onMouseEnter={(e) => e.target.style.backgroundColor = '#0056b3'}
+            onMouseLeave={(e) => e.target.style.backgroundColor = '#007bff'}
+          >
+            View My Portfolio
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
